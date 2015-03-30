bfbc39e15afbf2a12ab10038af2bd5a3b3a9227e
Ognjen Petrovic <ognjen@crobookings.com>
2015-03-30 07:34:45 -0400

    update

--- examples/example_1.js
+++ examples/example_1.js
@@ -2,6 +2,9 @@ var urlxml2json = require('../lib/urlxml2json');
 
 var x = new urlxml2json('http://www.xmlfiles.com/examples/cd_catalog.xml');
 x.on('end', function(json){
-	console.log(json);
+	json = JSON.parse(json);
+	for(var i=0; i < json.CATALOG.CD.length; ++i){
+		console.log(json.CATALOG.CD[i].TITLE);
+	}
 });
 x.get();
